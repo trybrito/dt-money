5fef27d8b3f9593173494da0279ef060d380d310
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2025-01-25 00:25:12 -0300

    styles: global styles + theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default.ts';
+import { GlobalStyle } from './styles/global.ts';
+
 export function App() {
-  return <h1>Hello, World</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello, World</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
